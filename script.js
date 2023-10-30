52c6d5d86c672e9b6843a4610e6fd3038b1fcc92
Louise Rondia <louiserondia@gmail.com>
2023-10-30 14:20:05 +0100

    trucs

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+
+window.mobileCheck = function () {
+	let check = false;
+	(function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
+};
+
+
 const menuContent = document.getElementById('menu-content');
 const menuLines = document.getElementById('menu-lines');
 const menuCross = document.getElementById('menu-cross');
@@ -8,7 +17,7 @@ const menuCross = document.getElementById('menu-cross');
 //fix petit bug quand l'animatione st en cours automatiquement et qu'on passe dessus
 
 {
-	if ('ontouchstart' in window || navigator.maxTouchPoints) {
+	if (window.mobileCheck() || 'ontouchstart' in window || navigator.maxTouchPoints) {
 		timeoutId = setTimeout(() => {
 			canvas.style.height = 'clamp(150px, 30%, 30%)';
 			canvas.style.transform = 'rotate(-35deg)';
@@ -118,7 +127,8 @@ charlotte.addEventListener('mouseleave', () => {
 
 // faire que sur gsm l'image apparaisse quand on passe en scrollant
 
-if ('ontouchstart' in window || navigator.maxTouchPoints) {
+
+if (window.mobileCheck() || 'ontouchstart' in window || navigator.maxTouchPoints) {
 	document.addEventListener("DOMContentLoaded", function () {
 		window.addEventListener("scroll", function () {
 			const image = charlotte;
@@ -141,7 +151,15 @@ const face1 = document.getElementById('face1');
 const triggerFace1 = document.getElementById('cateringTriggerFace');
 
 triggerFace1.addEventListener("click", function () {
-	window.addEventListener("scroll", function () {
+	if (isScrolledToDestination()) { //si on est déjà bien placé sur la page
+		face1.style.left = "1px";
+		setTimeout(() => {
+			window.addEventListener("scroll", function () {
+				face1.style.left = "-100px";
+			});
+		}, 200);
+	}
+	window.addEventListener("scroll", function () { // si on est loin, on scroll jusqu'au bon endroit, ensuite on fait apparaitre le perso, et ensuite on check si on scroll a nouveau pour l'enlever
 		if (isScrolledToDestination()) {
 			face1.style.left = "1px";
 			setTimeout(() => {
@@ -151,29 +169,30 @@ triggerFace1.addEventListener("click", function () {
 			}, 200);
 		}
 	});
-	face1.addEventListener("mouseenter", function () {
+	face1.addEventListener("click", function () { // si on click sur le perso, il se retire
 		face1.style.left = "-100px";
-		setTimeout(() => {
-			face1.style.left = "1px";
-		}, 250);
+		return;
 	});
+	// face1.addEventListener("mouseenter", function () {
+	// 	face1.style.left = "-100px";
+	// 	setTimeout(() => {
+	// 		face1.style.left = "1px";
+	// 	}, 250);
+	// });
 });
 
 function isScrolledToDestination() {
 	const destinationElement = document.querySelector('#catering'); // L'élément de destination
-	if (!destinationElement) return false; // Vérifiez s'il existe
+	if (!destinationElement) return false;
 
 	const elementRect = destinationElement.getBoundingClientRect();
 	return elementRect.top <= 50 && elementRect.top >= -50; // Vous pouvez ajuster la condition selon vos besoins
 }
 
-// cateringTriggerFace
-
 
 // -------------------
 //		  EMAIL
 // -------------------
-
 
 // Envoi d'un email avec le formulaire pour le catering
 
